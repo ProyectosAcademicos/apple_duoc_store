aa4d10a83680254bf205fe69ba6fc31bf090abcd
xDani-R <rodriguez.cesar725@gmail.com>
2026-09-18 14:47:14 -0300

    Subiendo apple-backend al repo

---
+++ apple-backend/src/main/java/cl/duoc/apple_backend/controller/AppleProductoController.java
@@ -0,0 +1,56 @@
+package cl.duoc.apple_backend.controller;
+
+import java.util.List;
+import java.util.Map;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AppleProductoController {
+
+    @GetMapping("api/productos")
+    public List<Map<String, Object>> listarProductos() {
+
+        return List.of(
+            Map.of (
+                "id", 1,
+                "nombre", "iPhone 13",
+                "categoria", "iPhone",
+                "precio", 1499990,
+                "stock", 10
+            ),
+
+            Map.of (
+                "id", 2,
+                "nombre", "MacBook Air",
+                "categoria", "Mac",
+                "precio", 1299990,
+                "stock", 8
+            ),
+
+            Map.of (
+                "id", 3,
+                "nombre", "iPad Pro",
+                "categoria", "iPad",
+                "precio", 999990,
+                "stock", 5
+            ),
+
+            Map.of (
+                "id", 4,
+                "nombre", "Apple Watch Series 7",
+                "categoria", "Apple Watch",
+                "precio", 499990,
+                "stock", 12
+            ),
+
+            Map.of (
+                "id", 5,
+                "nombre", "AirPods Pro",
+                "categoria", "AirPods",
+                "precio", 249990,
+                "stock", 15
+            )
+        );
+    }
+}
