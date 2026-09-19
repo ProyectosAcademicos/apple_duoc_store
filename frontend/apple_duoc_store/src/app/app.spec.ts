d0b625a87e8fe68aa7da326849df87180334cdbe
xDani-R <rodriguez.cesar725@gmail.com>
2026-09-19 19:24:49 -0300

    test: corregir titulo esperado de la tienda

--- frontend/apple_duoc_store/src/app/app.spec.ts
+++ frontend/apple_duoc_store/src/app/app.spec.ts
@@ -19,7 +19,7 @@ describe('App', () => {
     const fixture = TestBed.createComponent(App);
     await fixture.whenStable();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, apple_duoc_store');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Lo mejor de Apple.');
   });
 });
 
