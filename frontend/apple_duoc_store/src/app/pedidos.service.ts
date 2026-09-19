f62928c1228960044bf5e42a1a6b38a8e318550f
xDani-R <rodriguez.cesar725@gmail.com>
2026-09-19 20:13:30 -0300

    fix: recuperar configuracion funcional de Cognito y API

--- frontend/apple_duoc_store/src/app/pedidos.service.ts
+++ frontend/apple_duoc_store/src/app/pedidos.service.ts
@@ -18,7 +18,7 @@ export interface Producto {
 @Injectable ({ providedIn: 'root'})
 
 export class PedidosService{
-    private apiUrl = "https://psxwhfaf33.execute-api.us-east-1.amazonaws.com/test/api/productos";
+    private apiUrl = "https://jv7jt7mk3a.execute-api.us-east-1.amazonaws.com/test/api/producto";
     constructor(
         private http: HttpClient
     ){}
